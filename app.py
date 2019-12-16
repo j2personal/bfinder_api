d701ba052dfdfb28b8b00d39138ba8f12791d654
j2personal <junjang17@gmail.com>
2019-12-16 18:59:31 -0500

    fixing number

--- app.py
+++ app.py
@@ -4,14 +4,8 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    """Print 'Hello, world!' as the response body."""
-    return 'bfinder_api'
-
-@app.route('/send_notif')
-def send_message():
-    # Your Account SID from twilio.com/console
+def send_message(msg):
+	# Your Account SID from twilio.com/console
 	account_sid = os.environ.get("TWILIO_SID")
 	# Your Auth Token from twilio.com/console
 	auth_token  = os.environ.get("TWILIO_AUTH_TOKEN")
@@ -21,9 +15,26 @@ def send_message():
 	message = client.messages.create(
 	    to=os.environ.get("TO_NUMBER"), 
 	    from_=os.environ.get("FROM_NUMBER"),
-	    body="Notification Received!")
+	    body=msg)
 
 	return(message.sid)
+
+
+@app.route('/')
+def hello_world():
+    """Print 'Hello, world!' as the response body."""
+    return 'bfinder_api'
+
+@app.route('/send_vib')
+def send_vib():
+    send_message("Incoming Notification!")
+	return("Vib Sent")
+
+# @app.route('/request_juber', methods=["POST"])
+# def request_juber():
+#     request.form.get("")
+#     send_message("")
+# 	return("Vib Sent")
 
 
 
